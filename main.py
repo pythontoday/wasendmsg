df7dc66f61c897ddb3c307dc243284f4ce4c1cc1
pythontoday <pythontodaynet@gmail.com>
2022-04-17 13:47:59 +0700

    Changed main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import os
 
 
 def send_message_inst():
-    # mobile = input('Введите номер получателя: ')
-    mobile = os.getenv('MOBILE')
+    mobile = input('Введите номер получателя: ')
+    # mobile = os.getenv('MOBILE')
     message = input('Введите текст сообщения: ')
     
     pywhatkit.sendwhatmsg_instantly(phone_no=mobile, message=message)
     
     
 def send_message():
-    # mobile = input('Введите номер получателя: ')
-    mobile = os.getenv('MOBILE')
+    mobile = input('Введите номер получателя: ')
+    # mobile = os.getenv('MOBILE')
     message = input('Введите текст сообщения: ')
     hour = int(input('Введите часы: '))
     minutes = int(input('Введите минуты: '))
